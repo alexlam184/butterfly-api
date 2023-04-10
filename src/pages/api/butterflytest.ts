72fd6be168cb4bbe8c8d0c3b88d9e5dade171a88
alexlam184 <alexwork0211@gmail.com>
2023-04-10 19:29:49 +0800

    test 13123

--- src/pages/api/butterflytest.ts
+++ src/pages/api/butterflytest.ts
@@ -18,45 +18,46 @@ type RequestBodyType = {
 
 const date = dayjs().format();
 
-// const raw =
-//   '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
-
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const body: RequestBodyType = JSON.parse(req.body);
-  const myHeaders = new Headers();
-  myHeaders.append("api-key", "1EiJMNxAFieGuubW=TiRVN1kYGA=");
-  myHeaders.append("Content-Type", "text/plain");
+  try {
+    //const body: RequestBodyType = JSON.parse(req.body)
+    const myHeaders = new Headers();
+    myHeaders.append("api-key", "1EiJMNxAFieGuubW=TiRVN1kYGA=");
+    myHeaders.append("Content-Type", "text/plain");
 
-  // const raw =
-  //   '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
+    const raw =
+      '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
 
-  const raw = `{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "${dayjs().format(
-    "YYYY-MM-DDTHH:mm:ss"
-  )}",\r\n                    "value": "${
-    body.butterfly + "what"
-  }"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}`;
+    // const raw = `{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "${dayjs().format(
+    //   "YYYY-MM-DDTHH:mm:ss"
+    // )}",\r\n                    "value": "${
+    //   body.butterfly + "what"
+    // }"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}`;
 
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
 
-  fetch(
-    `http://api.onenet.hk.chinamobile.com/devices/161110960/datapoints`,
-    // @ts-ignore
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      res.status(200).json({ name: data });
-    })
-    .catch((error) => {
-      console.log("error", error);
-      res.status(200).json({ name: error });
-    });
+    fetch(
+      `http://api.onenet.hk.chinamobile.com/devices/161110960/datapoints`,
+      // @ts-ignore
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        res.status(200).json({ name: data });
+      })
+      .catch((error) => {
+        console.log("error", error);
+        res.status(200).json({ name: error });
+      });
+  } catch (e) {
+    res.status(200).json({ name: "fail:" + e });
+  }
 }
