8a5e9e58484c43fd99b9d9496e449239bde591f0
alexlam184 <alexwork0211@gmail.com>
2023-04-10 18:44:16 +0800

    test 2

---
+++ src/pages/api/butterflytest.ts
@@ -0,0 +1,56 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+// import utc from "dayjs/plugin/utc";
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Hong_Kong");
+
+type Data = {
+  name: string;
+};
+
+type RequestBodyType = {
+  butterfly: string;
+  deviceID: string;
+  apiKey: string;
+};
+
+const date = dayjs().format();
+
+// const raw =
+//   '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  //   const body: RequestBodyType = JSON.parse(req.body);
+  const myHeaders = new Headers();
+  myHeaders.append("api-key", "1EiJMNxAFieGuubW=TiRVN1kYGA=");
+  myHeaders.append("Content-Type", "text/plain");
+
+  const raw =
+    '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    `http://api.onenet.hk.chinamobile.com/devices/161110960/datapoints`,
+    // @ts-ignore
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.status(200).json({ name: data });
+    })
+    .catch((error) => {
+      console.log("error", error);
+      res.status(200).json({ name: error });
+    });
+}
