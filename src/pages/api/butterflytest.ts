c13b648eb26b1e86e83a7345e69dfe1b2bb08814
alexlam184 <alexwork0211@gmail.com>
2023-04-10 19:47:45 +0800

    trdt 23423

--- src/pages/api/butterflytest.ts
+++ src/pages/api/butterflytest.ts
@@ -24,7 +24,7 @@ export default function handler(
 ) {
   try {
     const body = req.body;
-    res.status(200).json(body);
+    res.status(200).json({ name: JSON.parse(body).butterfly });
     const myHeaders = new Headers();
     myHeaders.append("api-key", "1EiJMNxAFieGuubW=TiRVN1kYGA=");
     myHeaders.append("Content-Type", "text/plain");
