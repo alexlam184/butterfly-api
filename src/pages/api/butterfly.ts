6050c1644bf2a70cc5356a7d06cf5e9279e96856
alexlam184 <alexwork0211@gmail.com>
2023-04-10 20:40:19 +0800

    test eqads

--- src/pages/api/butterfly.ts
+++ src/pages/api/butterfly.ts
@@ -16,52 +16,66 @@ type RequestBodyType = {
   apiKey: string;
 };
 
-const date = dayjs().format();
-
-// const raw =
-//   '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
-
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const body: RequestBodyType = JSON.parse(req.body);
-  const myHeaders = new Headers();
-  //   myHeaders.append("api-key", body.apiKey);
-  myHeaders.append("api-key", "1EiJMNxAFieGuubW=TiRVN1kYGA=");
-  myHeaders.append("Content-Type", "text/plain");
+  //get current time
 
-  const raw = `{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "${dayjs().format(
-    "YYYY-MM-DDTHH:mm:ss"
-  )}",\r\n                    "value": "${
-    body.butterfly + "what"
-  }"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}`;
-
-  //   const raw =
-  //     '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
-  //   const requestOptions = {
-  //     method: "POST",
-  //     headers: myHeaders,
-  //     body: raw,
-  //     redirect: "follow",
-  //   };
-
-  //   fetch(
-  //     `http://api.onenet.hk.chinamobile.com/devices/${body.deviceID}/datapoints`,
-  //     // @ts-ignore
-  //     requestOptions
-  //   )
-  fetch(
-    `http://api.onenet.hk.chinamobile.com/devices/161110960/datapoints`,
-    // @ts-ignore
-    requestOptions
-  )
+  let currentTime: string = dayjs().format("YYYY-MM-DDTHH:mm:ss");
+  fetch(`http://worldtimeapi.org/api/timezone/Asia/Hong_Kong`)
     .then((response) => response.json())
     .then((data) => {
-      res.status(200).json({ name: data });
+      currentTime = dayjs(data.datetime).format("YYYY-MM-DDTHH:mm:ss");
     })
     .catch((error) => {
       console.log("error", error);
       res.status(200).json({ name: error });
     });
+
+  try {
+    const body = req.body;
+    let obj = null;
+    try {
+      obj = JSON.parse(body);
+    } catch (e) {
+      obj = {
+        butterfly: "default",
+        deviceID: "161110960",
+        apiKey: "1EiJMNxAFieGuubW=TiRVN1kYGA=",
+      };
+    }
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "text/plain");
+    myHeaders.append("api-key", body.apiKey);
+
+    // const raw =
+    //   '{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "2023-04-10T14:46:53",\r\n                    "value": "The latest butterfly123"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}';
+
+    const raw = `{\r\n    "datastreams": [{\r\n            "id": "butterfly",\r\n            "datapoints": [{\r\n                    "at": "${currentTime}",\r\n                    "value": "${body.butterfly}"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}`;
+
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      `http://api.onenet.hk.chinamobile.com/${body.deviceID}/161110960/datapoints`,
+      // @ts-ignore
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        res.status(200).json({ name: data });
+      })
+      .catch((error) => {
+        console.log("error", error);
+        res.status(200).json({ name: error });
+      });
+  } catch (e) {
+    res.status(200).json({ name: "fail:" + e });
+  }
 }
