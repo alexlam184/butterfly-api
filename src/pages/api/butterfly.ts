3c65ae76ea1e6bd833eb4ee3f2ee448151155d15
alexlam184 <alexwork0211@gmail.com>
2023-04-10 18:41:01 +0800

    no default

--- src/pages/api/butterfly.ts
+++ src/pages/api/butterfly.ts
@@ -26,8 +26,6 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   const body: RequestBodyType = JSON.parse(req.body);
-  res.status(200).json({ name: "success" });
-
   const myHeaders = new Headers();
   myHeaders.append("api-key", body.apiKey);
   myHeaders.append("Content-Type", "text/plain");
